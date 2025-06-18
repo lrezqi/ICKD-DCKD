61961c4076d0893bc618c716588d7637472ec246
lrezqi <67841851+lrezqi@users.noreply.github.com>
2025-06-18 02:00:57 +0100

    Update train_student.py

--- Cifar100/train_student.py
+++ Cifar100/train_student.py
@@ -76,6 +76,8 @@ def parse_option():
                                  'resnet8x4', 'resnet32x4', 'wrn_16_1', 'wrn_16_2', 'wrn_40_1', 'wrn_40_2',
                                  'vgg8', 'vgg11', 'vgg13', 'vgg16', 'vgg19', 'ResNet50',
                                  'MobileNetV2', 'ShuffleV1', 'ShuffleV2'])
+    parser.add_argument('--model_t', type=str, required=True, help='teacher model')
+
     parser.add_argument('--path_t', type=str, default=None, help='teacher model snapshot')
 
     # distillation
