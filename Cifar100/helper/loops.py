b20a461cc3de26bee33cb0824b5dbcbef5615ab9
lrezqi <67841851+lrezqi@users.noreply.github.com>
2025-06-20 21:34:53 +0100

    Update loops.py

--- Cifar100/helper/loops.py
+++ Cifar100/helper/loops.py
@@ -1,11 +1,14 @@
-from __future__ import print_function, division
+
+from __future__ import division, print_function
 
 import sys
 import time
+
 import torch
 
-from .util import AverageMeter, accuracy
 from .feature_visualization import FeatureVisualizer
+from .util import AverageMeter, accuracy
+
 
 def train_vanilla(epoch, train_loader, model, criterion, optimizer, opt):
     """vanilla training"""
@@ -49,18 +52,26 @@ def train_vanilla(epoch, train_loader, model, criterion, optimizer, opt):
 
         # print info
         if idx % opt.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                   epoch, idx, len(train_loader), batch_time=batch_time,
-                   data_time=data_time, loss=losses, top1=top1, top5=top5))
+            print(
+                "Epoch: [{0}][{1}/{2}]\t"
+                "Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t"
+                "Data {data_time.val:.3f} ({data_time.avg:.3f})\t"
+                "Loss {loss.val:.4f} ({loss.avg:.4f})\t"
+                "Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t"
+                "Acc@5 {top5.val:.3f} ({top5.avg:.3f})".format(
+                    epoch,
+                    idx,
+                    len(train_loader),
+                    batch_time=batch_time,
+                    data_time=data_time,
+                    loss=losses,
+                    top1=top1,
+                    top5=top5,
+                )
+            )
             sys.stdout.flush()
 
-    print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-          .format(top1=top1, top5=top5))
+    print(f" * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}")
 
     return top1.avg, losses.avg
 
@@ -73,9 +84,9 @@ def train_distill(epoch, train_loader, module_list, criterion_list, optimizer, o
     # set teacher as eval()
     module_list[-1].eval()
 
-    if opt.distill == 'abound':
+    if opt.distill == "abound":
         module_list[1].eval()
-    elif opt.distill == 'factor':
+    elif opt.distill == "factor":
         module_list[2].eval()
 
     criterion_cls = criterion_list[0]
@@ -92,7 +103,7 @@ def train_distill(epoch, train_loader, module_list, criterion_list, optimizer, o
 
     end = time.time()
     for idx, data in enumerate(train_loader):
-        if opt.distill in ['crd']:
+        if opt.distill in ["crd"]:
             input, target, index, contrast_idx = data
         else:
             input, target, index = data
@@ -103,12 +114,12 @@ def train_distill(epoch, train_loader, module_list, criterion_list, optimizer, o
             input = input.cuda()
             target = target.cuda()
             index = index.cuda()
-            if opt.distill in ['crd']:
+            if opt.distill in ["crd"]:
                 contrast_idx = contrast_idx.cuda()
 
         # ===================forward=====================
         preact = False
-        if opt.distill in ['abound']:
+        if opt.distill in ["abound"]:
             preact = True
         feat_s, logit_s = model_s(input, is_feat=True, preact=preact)
         with torch.no_grad():
@@ -118,77 +129,77 @@ def train_distill(epoch, train_loader, module_list, criterion_list, optimizer, o
         loss_cls = criterion_cls(logit_s, target)
         loss_div = criterion_div(logit_s, logit_t)
         # other kd beyond KL divergence
-        if opt.distill == 'kd':
+        if opt.distill == "kd":
             loss_kd = 0
-        elif opt.distill == 'afd':
+        elif opt.distill == "afd":
             loss_kd = criterion_kd(feat_s, feat_t)
-        elif opt.distill == 'hint':
+        elif opt.distill == "hint":
             f_s = module_list[1](feat_s[opt.hint_layer])
             f_t = feat_t[opt.hint_layer]
             loss_kd = criterion_kd(f_s, f_t)
-        elif opt.distill == 'crd':
+        elif opt.distill == "crd":
             f_s = feat_s[-1]
             f_t = feat_t[-1]
             loss_kd = criterion_kd(f_s, f_t, index, contrast_idx)
             g_s = [feat_s[-2]]
             g_t = [feat_t[-2]]
-        elif opt.distill == 'attention':
+        elif opt.distill == "attention":
             g_s = feat_s[1:-1]
             g_t = feat_t[1:-1]
             loss_group = criterion_kd(g_s, g_t)
             loss_kd = sum(loss_group)
-        elif opt.distill == 'nst':
+        elif opt.distill == "nst":
             g_s = feat_s[1:-1]
             g_t = feat_t[1:-1]
             loss_group = criterion_kd(g_s, g_t)
             loss_kd = sum(loss_group)
-        elif opt.distill == 'similarity':
+        elif opt.distill == "similarity":
             g_s = [feat_s[-2]]
             g_t = [feat_t[-2]]
             loss_group = criterion_kd(g_s, g_t)
             loss_kd = sum(loss_group)
-        elif opt.distill == 'ickd':
+        elif opt.distill == "ickd":
             g_s = [feat_s[-2]]
             g_t = [feat_t[-2]]
             loss_group = criterion_kd(g_s, g_t)
             loss_kd = sum(loss_group)
-        elif opt.distill == 'rkd':
+        elif opt.distill == "rkd":
             f_s = feat_s[-1]
             f_t = feat_t[-1]
             loss_kd = criterion_kd(f_s, f_t)
-        elif opt.distill == 'pkt':
+        elif opt.distill == "pkt":
             f_s = feat_s[-1]
             f_t = feat_t[-1]
             loss_kd = criterion_kd(f_s, f_t)
-        elif opt.distill == 'kdsvd':
+        elif opt.distill == "kdsvd":
             g_s = feat_s[1:-1]
             g_t = feat_t[1:-1]
             loss_group = criterion_kd(g_s, g_t)
             loss_kd = sum(loss_group)
-        elif opt.distill == 'correlation':
+        elif opt.distill == "correlation":
             f_s = module_list[1](feat_s[-1])
             f_t = module_list[2](feat_t[-1])
             loss_kd = criterion_kd(f_s, f_t)
-        elif opt.distill == 'vid':
+        elif opt.distill == "vid":
             g_s = feat_s[1:-1]
             g_t = feat_t[1:-1]
             loss_group = [c(f_s, f_t) for f_s, f_t, c in zip(g_s, g_t, criterion_kd)]
             loss_kd = sum(loss_group)
-        elif opt.distill == 'abound':
+        elif opt.distill == "abound":
             # can also add loss to this stage
             loss_kd = 0
-        elif opt.distill == 'fsp':
+        elif opt.distill == "fsp":
             # can also add loss to this stage
             loss_kd = 0
-        elif opt.distill == 'factor':
+        elif opt.distill == "factor":
             factor_s = module_list[1](feat_s[-2])
             factor_t = module_list[2](feat_t[-2], is_factor=True)
             loss_kd = criterion_kd(factor_s, factor_t)
-        elif opt.distill == 'dckd':
+        elif opt.distill == "dckd":
             # Exemple minimal, à adapter selon le papier / code original
             model_s.train()
             model_t.eval()
-            
+
             for batch_idx, (input, target, _, index) in enumerate(train_loader):
                 input = input.cuda()
                 target = target.cuda()
@@ -196,13 +207,13 @@ def train_distill(epoch, train_loader, module_list, criterion_list, optimizer, o
                 feat_s, logit_s = model_s(input, is_feat=True)
                 with torch.no_grad():
                     feat_t, logit_t = model_t(input, is_feat=True)
-                    
+
                 loss_cls = criterion_cls(logit_s, target)
                 loss_div = criterion_div(logit_s, logit_t)
                 loss_kd = criterion_kd(feat_s, feat_t)
-                
+
                 loss = opt.gamma * loss_cls + opt.alpha * loss_div + opt.beta * loss_kd
-                
+
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
@@ -225,18 +236,26 @@ def train_distill(epoch, train_loader, module_list, criterion_list, optimizer, o
 
         # print info
         if idx % opt.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                epoch, idx, len(train_loader), batch_time=batch_time,
-                data_time=data_time, loss=losses, top1=top1, top5=top5))
+            print(
+                "Epoch: [{0}][{1}/{2}]\t"
+                "Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t"
+                "Data {data_time.val:.3f} ({data_time.avg:.3f})\t"
+                "Loss {loss.val:.4f} ({loss.avg:.4f})\t"
+                "Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t"
+                "Acc@5 {top5.val:.3f} ({top5.avg:.3f})".format(
+                    epoch,
+                    idx,
+                    len(train_loader),
+                    batch_time=batch_time,
+                    data_time=data_time,
+                    loss=losses,
+                    top1=top1,
+                    top5=top5,
+                )
+            )
             sys.stdout.flush()
 
-    print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-          .format(top1=top1, top5=top5))
+    print(f" * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}")
 
     return top1.avg, losses.avg
 
@@ -274,15 +293,21 @@ def validate(val_loader, model, criterion, opt):
             end = time.time()
 
             if idx % opt.print_freq == 0:
-                print('Test: [{0}/{1}]\t'
-                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                      'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                      'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                       idx, len(val_loader), batch_time=batch_time, loss=losses,
-                       top1=top1, top5=top5))
+                print(
+                    "Test: [{0}/{1}]\t"
+                    "Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t"
+                    "Loss {loss.val:.4f} ({loss.avg:.4f})\t"
+                    "Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t"
+                    "Acc@5 {top5.val:.3f} ({top5.avg:.3f})".format(
+                        idx,
+                        len(val_loader),
+                        batch_time=batch_time,
+                        loss=losses,
+                        top1=top1,
+                        top5=top5,
+                    )
+                )
 
-        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-              .format(top1=top1, top5=top5))
+        print(f" * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}")
 
     return top1.avg, top5.avg, losses.avg
