8364ae8b23e22bad6eecfbf1b95689d51b6d3b9e
lrezqi <67841851+lrezqi@users.noreply.github.com>
2025-06-20 17:38:59 +0100

    Update loops.py

--- Cifar100/helper/loops.py
+++ Cifar100/helper/loops.py
@@ -185,6 +185,26 @@ def train_distill(epoch, train_loader, module_list, criterion_list, optimizer, o
             factor_s = module_list[1](feat_s[-2])
             factor_t = module_list[2](feat_t[-2], is_factor=True)
             loss_kd = criterion_kd(factor_s, factor_t)
+        elif opt.distill == 'dckd':
+        # Exemple minimal, à adapter selon le papier / code original
+        for batch_idx, (input, target, _, index) in enumerate(train_loader):
+            input = input.cuda()
+            target = target.cuda()
+
+            # Forward pass
+            feat_s, logit_s = model_s(input, is_feat=True)
+            with torch.no_grad():
+                feat_t, logit_t = model_t(input, is_feat=True)
+
+            loss_cls = criterion_cls(logit_s, target)
+            loss_div = criterion_div(logit_s, logit_t)
+            loss_kd = criterion_kd(feat_s, feat_t)
+
+            loss = opt.gamma * loss_cls + opt.alpha * loss_div + opt.beta * loss_kd
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
         else:
             raise NotImplementedError(opt.distill)
         loss = opt.gamma * loss_cls + opt.alpha * loss_div + opt.beta * loss_kd
