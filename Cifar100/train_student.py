692a73da583604b67e932fe22edd12b7cf12a34d
lrezqi <67841851+lrezqi@users.noreply.github.com>
2025-06-20 01:48:11 +0100

    Update train_student.py

--- Cifar100/train_student.py
+++ Cifar100/train_student.py
@@ -320,14 +320,13 @@ def main():
         init(model_s, model_t, init_trainable_list, criterion_kd, train_loader, logger, opt)
         # classification training
         pass
-     elif opt.distill == 'dckd':
+    elif opt.distill == 'dckd':
         opt.s_dim = feat_s[-2].shape[1]
         opt.t_dim = feat_t[-2].shape[1]
         opt.feat_dim = opt.t_dim
         criterion_kd = DCKDLoss(opt)
         module_list.append(criterion_kd)
         trainable_list.append(criterion_kd)
-
 
     else:
         raise NotImplementedError(opt.distill)
