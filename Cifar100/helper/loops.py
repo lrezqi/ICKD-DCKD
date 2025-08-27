7cbc75258334172ce546d42ce772153379e34496
lrezqi <67841851+lrezqi@users.noreply.github.com>
2025-08-27 22:37:58 +0100

    Update loops.py

--- Cifar100/helper/loops.py
+++ Cifar100/helper/loops.py
@@ -219,11 +219,9 @@ def train_distill(epoch, train_loader, module_list, criterion_list, optimizer, o
         top5.update(acc5[0], input.size(0))
                 # ===================backward=====================
         # Mesure du temps
-        optimizer.zero_grad()
         batch_time.update(time.time() - end)
-        loss.backward(retain_graph=True)
         end = time.time()
-        optimizer.step()
+    
         # Affichage
         if idx % opt.print_freq == 0:
             print(
@@ -235,42 +233,9 @@ def train_distill(epoch, train_loader, module_list, criterion_list, optimizer, o
                 f"Acc@5 {top5.val:.3f} ({top5.avg:.3f})"
             )
             sys.stdout.flush()
+            
     print(f" * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}")
     return top1.avg, losses.avg
- 
-        # ===================backward=====================
-     #  optimizer.zero_grad()
-     #  loss.backward(retain_graph=True)
-     #  optimizer.step()
-        # ===================meters=====================
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        # print info
-        if idx % opt.print_freq == 0:
-            print(
-                "Epoch: [{0}][{1}/{2}]\t"
-                "Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t"
-                "Data {data_time.val:.3f} ({data_time.avg:.3f})\t"
-                "Loss {loss.val:.4f} ({loss.avg:.4f})\t"
-                "Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t"
-                "Acc@5 {top5.val:.3f} ({top5.avg:.3f})".format(
-                    epoch,
-                    idx,
-                    len(train_loader),
-                    batch_time=batch_time,
-                    data_time=data_time,
-                    loss=losses,
-                    top1=top1,
-                    top5=top5,
-                )
-            )
-            sys.stdout.flush()
-
-    print(f" * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}")
-
-    return top1.avg, losses.avg
-
 
 def validate(val_loader, model, criterion, opt):
     """validation"""
